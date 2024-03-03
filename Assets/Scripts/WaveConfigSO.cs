e3ffd2046c02ba6177b35319b54c36a97eee52e1
Brian Egan <brianjoseph.egan@gmail.com>
2024-03-03 10:52:05 -0800

    Spawning Enemies
    Add EnemySpawner GameObject and script.

--- Assets/Scripts/WaveConfigSO.cs
+++ Assets/Scripts/WaveConfigSO.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
 public class WaveConfigSO : ScriptableObject
 {
+    [SerializeField] List<GameObject> enemyPrefabs;
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 5f;
 
     public float GetMoveSpeed() { return moveSpeed; }
 
     public Transform GetStartingWaypoint() { return pathPrefab.GetChild(0); }
+
+    public int GetEnemyCount() { return enemyPrefabs.Count; }
+
+    public GameObject GetEnenyPrefab(int index) { return enemyPrefabs[index]; }
 
     public List<Transform> GetWaypoints()
     {
