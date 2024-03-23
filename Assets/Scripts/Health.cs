99b9aa6409c968d3ade8faddd53b053b873112ac
Brian Egan <brianjoseph.egan@gmail.com>
2024-03-23 11:54:57 -0700

    Particle Effects
    Add particle effects that imitate explosions when player or enemy takes damage.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] int health = 50;
+    [SerializeField] ParticleSystem hitEffect;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<DamageDealer>() is DamageDealer damageDealer)
         {
             TakeDamage(damageDealer.GetDamage());
+            PlayHitEffect();
             damageDealer.Hit();
         }
     }
@@ -21,6 +23,15 @@ public class Health : MonoBehaviour
         if (health <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+
+    void PlayHitEffect()
+    {
+        if (hitEffect != null)
+        {
+            ParticleSystem instance = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
         }
     }
 }
